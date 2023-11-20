3e52e0b9f4012bf1f58019172657c28c70eb3fcd
Rafael Julio <rafaeljuliodsilveira212@gmail.com>
2023-11-20 16:09:49 -0300

    Shopping list project. FrontEnd using Flutter and a simple FireBase backend

---
+++ lib/models/categories_enum.dart
@@ -0,0 +1,13 @@
+
+enum Categories {
+  vegetables,
+  fruit,
+  meat,
+  dairy,
+  carbs,
+  sweets,
+  spices,
+  convenience,
+  hygiene,
+  other,
+}
---
+++ lib/widgets/list_item.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_list/models/grocery_item.dart';
+
+class ListItem extends StatelessWidget {
+  const ListItem({super.key, required this.item});
+
+  final GroceryItem item;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+            title: Text(item.name),
+            leading: Container(
+              width: 24,
+              height: 24,
+              color: item.category.color,
+            ),
+            trailing: Text(
+              item.quantity.toString(),
+            )
+    );
+  }
+}
